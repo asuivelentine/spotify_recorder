b4d71f4490a583e57edcdce942913f39279d321a
neocogent <info@neocogent.com>
2020-12-25 09:21:52 +1000

    new parsing of metadata

--- recorder.sh
+++ recorder.sh
@@ -56,16 +56,12 @@ while true; do
     spotify_metadata=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify \
         /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get \
         string:org.mpris.MediaPlayer2.Player string:Metadata 2>/dev/null)
-
-    artist=$(echo $spotify_metadata | grep -o 'artist.*autoRating' | grep -o '"[a-zA-Z0-9].* ]' | tr -d '"]')
-    albumartist=$(echo $spotify_metadata | grep -o 'albumArtist.*artist' | grep -o '"[a-zA-Z0-9].* ]' | tr -d '"]')
-    album=$(echo $spotify_metadata | grep -o 'album.*albumArtist' | grep -o '"[a-zA-Z0-9].*)' | tr -d '")' | tr '/' '-')
-    title=$(echo $spotify_metadata | grep -o 'title.*trackNumber' | grep -o '"[a-zA-Z0-9].*)' | tr -d '")' | tr '/' '-')
-    trk=$(echo $spotify_metadata | grep -o 'trackNumber.*url' | grep -o ' [0-9].*)' | tr -d ' )')
-    album=${album%% }
-    artist=${artist%% }
-    albumartist=${albumartist%% }
-    title=${title%% }
+	readarray -t data <<<$(echo $spotify_metadata | grep -Eo '"[^"]*"| [0-9]+' | tr -d '"' | tr '/' '-')
+	album=${data[7]}
+	albumartist=${data[9]}
+	artist=${data[11]}
+	title=${data[17]}
+	trk=${data[19]}
     printf -v track "%02d" $trk
 
     if [ -z "$artist" ]; then
